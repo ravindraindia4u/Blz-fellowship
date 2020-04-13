911450e500235b148d7272ba28d2f70e5ed1d264
ravindraindia4u <40546374+ravindraindia4u@users.noreply.github.com>
2020-04-14 04:10:11 +0530

    Delete random.sh

--- Week-1/02.Sequences/random.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash -x
-
-echo "Random Number: "$(( RANDOM ))
-echo "Random Number B/W 1 to 10 : "$((1 +  RANDOM % 10 ))
-echo "Random Number B/W 0 to 1 : "$(( RANDOM % 2 ))
-echo "Random Number B/W 1 to 6 : "$((1 + RANDOM % 6 ))
