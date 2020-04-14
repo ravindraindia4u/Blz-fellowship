456a23558a4d1a21db15c81b02692f0a9d31aa34
ravindraindia4u <ravindraindia4u@gmail.com>
2020-04-14 12:18:45 +0530

    Checking Employee is Present or Absent

--- Week-2/empWageComputation.sh
+++ Week-2/empWageComputation.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
 echo "		~: Welcome to Employee Wage Computation Program :~";
+
+#CONSTANTS FOR THE PROGRAM
+PRESENT=1;
+ABSENT=0;
+
+attendanceCheck=$((RANDOM%2))
+if [ $PRESENT -eq $attendanceCheck ]
+then
+	echo "Employee is Present";
+else
+	echo "Employee is Absent";
+fi
