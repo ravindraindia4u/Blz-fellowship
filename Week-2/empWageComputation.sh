497307132b69b0903bdc080227233a6bca55b8aa
ravindraindia4u <ravindraindia4u@gmail.com>
2020-04-14 15:06:25 +0530

    Solving Using Case Statements

--- Week-2/empWageComputation.sh
+++ Week-2/empWageComputation.sh
@@ -5,24 +5,26 @@ echo "		~: Welcome to Employee Wage Computation Program :~";
 PRESENT=1;
 ABSENT=0;
 WAGE_PER_HR=20;
-IS_FULL_TIME=8;
-IS_PART_TIME=4;
+IS_FULL_TIME=1;
+IS_PART_TIME=0;
 IS_ABSENT=0;
 
+#VARIABLES
+empHours=0;
 attendanceCheck=$((RANDOM%2))
 if [ $PRESENT -eq $attendanceCheck ]
 then
 	empCheck=$((RANDOM%2))
-	if [ $empCheck -eq 1 ]
-	then
-		echo "Employee is Present - Full Time"
-		dailyWage=$(($WAGE_PER_HR * $IS_FULL_TIME))
-		echo "Daily Wage = "$dailyWage;
-	else
-		echo "Employee is Present - Part Time"
-		dailyWage=$(($WAGE_PER_HR * $IS_PART_TIME))
-		echo "Daily Wage = "$dailyWage;
-	fi
+	case $empCheck in
+		$IS_FULL_TIME)
+			echo "Employee is Present - Full Time";
+			empHours=8;;
+		$IS_PART_TIME)
+			echo "Employe is Present - Part Time";
+			empHours=4;;
+	esac
+	dailyWage=$(($WAGE_PER_HR * $empHours))
+	echo "Daily Wage = "$dailyWage;
 else
 	echo "Employee is Absent";
 	dailyWage=$(($WAGE_PER_HR * $IS_ABSENT))
