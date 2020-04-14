752b21c65488c46a25a5fc2d140ce70258abb653
ravindraindia4u <ravindraindia4u@gmail.com>
2020-04-14 13:33:05 +0530

    Adding part time employee & wage

--- Week-2/empWageComputation.sh
+++ Week-2/empWageComputation.sh
@@ -6,16 +6,26 @@ PRESENT=1;
 ABSENT=0;
 WAGE_PER_HR=20;
 IS_FULL_TIME=8;
+IS_PART_TIME=4;
 IS_ABSENT=0;
 
 attendanceCheck=$((RANDOM%2))
 if [ $PRESENT -eq $attendanceCheck ]
 then
-	echo "Employee is Present";
-	dailyWage=$(($WAGE_PER_HR * $IS_FULL_TIME))
-	echo "Daily Wage = "$dailyWage;
+	empCheck=$((RANDOM%2))
+	if [ $empCheck -eq 1 ]
+	then
+		echo "Employee is Present - Full Time"
+		dailyWage=$(($WAGE_PER_HR * $IS_FULL_TIME))
+		echo "Daily Wage = "$dailyWage;
+	else
+		echo "Employee is Present - Part Time"
+		dailyWage=$(($WAGE_PER_HR * $IS_PART_TIME))
+		echo "Daily Wage = "$dailyWage;
+	fi
 else
 	echo "Employee is Absent";
 	dailyWage=$(($WAGE_PER_HR * $IS_ABSENT))
 	echo "Daily Wage = "$dailyWage;
 fi
+
