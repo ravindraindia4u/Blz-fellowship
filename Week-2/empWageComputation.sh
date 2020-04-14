0a0d52dd443be28f98312f0984c46af5771e2993
ravindraindia4u <ravindraindia4u@gmail.com>
2020-04-14 16:57:52 +0530

    UC-6-CalulatingWageTillTotalWorkingHrsOrDayIsReachedForAMonth

--- Week-2/empWageComputation.sh
+++ Week-2/empWageComputation.sh
@@ -5,6 +5,7 @@ echo "		~: Welcome to Employee Wage Computation Program :~";
 PRESENT=1;
 ABSENT=0;
 WAGE_PER_HR=20;
+MAX_HRS_IN_MONTH=100;
 IS_FULL_TIME=2;
 IS_PART_TIME=1;
 IS_ABSENT=0;
@@ -16,8 +17,10 @@ fullDayWork=0;
 halfDayWork=0;
 absent=0;
 totalEmpHours=0;
-for (( day=1; day<=$NUM_WORKING_DAYS; day++ ))
+totalWorkingDays=0;
+while [[ $totalEmpHours -lt $MAX_HRS_IN_MONTH && $totalWorkingDays -lt $NUM_WORKING_DAYS ]]
 do
+	((totalWorkingDays++))
 	empCheck=$((RANDOM%3))
 	case $empCheck in
 		$IS_FULL_TIME)
